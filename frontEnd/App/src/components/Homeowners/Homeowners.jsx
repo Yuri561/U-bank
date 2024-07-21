f37790d23a33fec9ff291ede750089fe3af91edb
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-07-21 11:47:13 -0400

    added nav on homeowner

--- frontEnd/App/src/components/Homeowners/Homeowners.jsx
+++ frontEnd/App/src/components/Homeowners/Homeowners.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Container, Box, Typography, Paper, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import heroImage from '../../img/hero-image.jpg'; // Ensure the path is correct
 import './Homeowners.css';
 
 const HeroSection = styled(Box)(({ theme }) => ({
   position: 'relative',
   width: '100%',
-  height: '400px',
+  height: '550px',
   background: `url(${heroImage}) no-repeat center center`,
   backgroundSize: 'cover',
   color: '#fff',
@@ -32,38 +34,113 @@ const CardSection = styled('section')(({ theme }) => ({
 }));
 
 const StepsContainer = styled(Paper)(({ theme }) => ({
-  position: 'absolute',
-  bottom: '-25px',
-  left: '50%',
-  transform: 'translateX(-50%)',
-  width: '90%',
-  padding: theme.spacing(2),
-  display: 'flex',
-  justifyContent: 'space-between',
-  backgroundColor: '#fff',
-  overflowX: 'auto',
-  whiteSpace: 'nowrap',
+	position: 'absolute',
+	bottom: '-25px',
+	left: '50%',
+	transform: 'translateX(-50%)',
+	width: '90%',
+	padding: theme.spacing(2),
+	display: 'flex',
+	justifyContent: 'space-between',
+	background: 'linear-gradient(to bottom, #a8c0ff, #3f2b96)',
+	overflowX: 'auto',
+	whiteSpace: 'nowrap',
 }));
 
-const StepBox = styled(Box)(({ theme }) => ({
-  flex: '1 0 auto',
-  textAlign: 'center',
-  padding: theme.spacing(1),
-  cursor: 'pointer',
-  '&:not(:last-child)': {
-    borderRight: `1px solid ${theme.palette.divider}`,
-  },
+const StepBox = styled(Box)(({ theme, active }) => ({
+	flex: '1 0 auto',
+	textAlign: 'center',
+	padding: theme.spacing(1),
+	cursor: 'pointer',
+	position: 'relative', // Add this line to position the icon
+	borderBottom: active ? `2px solid ${theme.palette.primary.main}` : 'none',
+	'&:not(:last-child)': {
+		borderRight: `1px solid ${theme.palette.divider}`,
+	},
+	'&:hover': {
+		borderRadius: '5px',
+		transform: 'scale(1.1)',
+		boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)',
+		background: 'linear-gradient(to right, #f0f0f0, #ffff)',
+		'& .chevron-icon': {
+			// Add this to show the icon on hover
+			display: 'block',
+		},
+	},
+}));
+
+const ChevronIcon = styled(ExpandMoreIcon)(({ theme }) => ({
+	position: 'absolute',
+	bottom: '5px',
+	right: '5px',
+	display: 'none', // Initially hide the icon
 }));
 
 const stepContent = [
-  "Content for what can you afford goes here.",
-  "Content for down payments and PMI goes here.",
-  "Content for finding your team goes here.",
-  "Content for finding the right home goes here.",
-  "Content for working with your lender goes here.",
-  "Content for making your offer goes here.",
-  "Content for inspecting and appraising goes here.",
-  "Content for closing your loan goes here."
+  {
+    title: "What can you afford",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">What can you afford content goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Down payment and PMI",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for down payments and PMI goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Find your team",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for finding your team goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Finding the right home",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for finding the right home goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Working with your lender",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for working with your lender goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Making your offer",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for making your offer goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Inspecting and Appraising",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for inspecting and appraising goes here.</Typography>
+      </div>
+    )
+  },
+  {
+    title: "Closing on your loan",
+    content: (
+      <div className='p-6 mx-auto flex flex-col justify-center items-center'>
+        <Typography variant="body1">Content for closing your loan goes here.</Typography>
+      </div>
+    )
+  }
 ];
 
 const stepHeader =[
@@ -82,25 +159,28 @@ function Homeowners() {
   const [activeStep, setActiveStep] = useState(0);
 
   return (
-    <div>
-      <HeroSection>
-     
-      </HeroSection>
-      <StepsContainer elevation={3} className='mb-[20rem]'>
-        {stepContent.map((step, index) => (
-          <StepBox key={index} onClick={() => setActiveStep(index)}>
-            <Typography variant="h6">STEP {index + 1}</Typography>
-            <Typography>{step.split(' ')}</Typography>
-          </StepBox>
-        ))}
-      </StepsContainer>
-      <Box mt={25} className='card-content'>
-        <CardSection className='card-section visible'>
-          <Typography variant="h2">{stepContent[activeStep]}</Typography>
-        </CardSection>
-      </Box>
-    </div>
-  );
+		<div>
+			<HeroSection>
+		
+			</HeroSection>
+			<StepsContainer elevation={3} className='mb-[20rem]'>
+				{stepHeader.map((step, index) => (
+					<StepBox
+						key={index}
+						onClick={() => setActiveStep(index)}
+						active={activeStep === index}>
+						<Typography variant='h6'>STEP {index + 1}</Typography>
+						<Typography>{step}</Typography>
+					</StepBox>
+				))}
+			</StepsContainer>
+			<Box mt={25} className='card-content '>
+				<CardSection className='card-section visible h-screen'>
+					<Typography variant='h2'>{stepContent[activeStep].content}</Typography>
+				</CardSection>
+			</Box>
+		</div>
+	);
 }
 
 export default Homeowners;
